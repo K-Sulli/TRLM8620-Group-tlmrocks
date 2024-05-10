411cc0edbf95dbf5858452d7652cefd4d088d43f
jmontedoro <jmontedoro@middlebury.edu>
2024-05-09 21:55:16 -0700

    French localization
    French localization

--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -3,22 +3,22 @@ import i18n from "../../services/i18n.js";
 
 let Home = {
     render : async () => {
-        //fetch locale-sensitive strings via i18n method
-        let welcomeSubtitle = i18n.getString("Home", "welcomeSubtitle");
+        // Fetch locale-sensitive strings via i18n method
+        let welcomeTitle = i18n.getString("Home", "welcomeTitle"); // Fetching the externalized welcome title
+        let welcomeSubtitle = i18n.getString("Home", "welcomeSubtitle"); // Fetching the externalized subtitle
 
-        //view is solely for HTML markup, contains no static text
+        // View is solely for HTML markup, contains no static text
         let view = `
                     <section class="welcome">
-                        <h1 class="center">Welcome to Pedego!</h1>
-                        <h3 class="center white">${welcomeSubtitle}</h3>
+                        <h1 class="center">${welcomeTitle}</h1>  <!-- Using the externalized title -->
+                        <h3 class="center">${welcomeSubtitle}</h3> <!-- Using the externalized subtitle -->
                     </section>
                     <div class="browseGrid homeGrid">`;
 
-        //create a box to display each of the 4 featured products
+        // Create a box to display each of the 4 featured products
         featuredProducts.forEach((product, key) => {
-
-            //string to give image an alt tag for accessibility
-            let imageAlt = product.title + " image";
+            // String to give image an alt tag for accessibility
+            let imageAlt = i18n.getString("Home", "symbolAlt"); // Fetching alt text for images
 
             view += `
                     <article id="${product.productID}" class="${product.type}">
@@ -34,22 +34,20 @@ let Home = {
             
         view += "</div>";
 
-        //return generated markup
-        return view
-    }
-    , after_render: async () => {
+        // Return generated markup
+        return view;
+    },
+    after_render: async () => {
         let articles = document.querySelectorAll("article");
 
-        //click listener to redirect on product click
+        // Click listener to redirect on product click
         for(let curProduct of articles) {
             curProduct.addEventListener("click", function() {
                 location.href=`./#/${curProduct.classList[0]}s/` + curProduct.id;
             }, false);
             curProduct.classList.add("zoom");
         }
-
     }
-
 }
 
 export default Home;
